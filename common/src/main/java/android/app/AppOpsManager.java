5ee06ef27a3105497e5fcf2b938457178f3c3c81
Nihility <Nihility.t@gmail.com>
2022-08-12 21:48:34 +0800

    merge common and extenders library

--- common/src/main/java/android/app/AppOpsManager.java
+++ common/src/main/java/android/app/AppOpsManager.java
@@ -5,10 +5,11 @@
 
 package android.app;
 
+import static android.os.Build.VERSION_CODES.N;
+
 import android.annotation.TargetApi;
 import android.app.usage.UsageStatsManager;
-
-import static android.os.Build.VERSION_CODES.N;
+import android.os.IBinder;
 
 public class AppOpsManager {
     public static final int MODE_ALLOWED = 0;
@@ -16,14 +17,20 @@ public class AppOpsManager {
     public static final int MODE_ERRORED = 2;
     public static final int MODE_IGNORED = 1;
 
-    /** @hide Control whether an application is allowed to run in the background. */
+    /**
+     * @hide Control whether an application is allowed to run in the background.
+     */
     @TargetApi(N)
     public static final int OP_RUN_IN_BACKGROUND = 63;
 
-    /** @hide */
+    /**
+     * @hide
+     */
     public static final int OP_POST_NOTIFICATION = 11;
 
-    /** @hide Retrieve current usage stats via {@link UsageStatsManager}. */
+    /**
+     * @hide Retrieve current usage stats via {@link UsageStatsManager}.
+     */
     public static final int OP_GET_USAGE_STATS = 43;
 
     public static final String OPSTR_ADD_VOICEMAIL = "android:add_voicemail";
@@ -61,11 +68,7 @@ public class AppOpsManager {
     public static final String OPSTR_WRITE_EXTERNAL_STORAGE = "android:write_external_storage";
     public static final String OPSTR_WRITE_SETTINGS = "android:write_settings";
 
-    AppOpsManager() {
-        throw new RuntimeException("Stub!");
-    }
-
-    public int checkOpNoThrow(int op, int uid, String packageName) {
+    public AppOpsManager() {
         throw new RuntimeException("Stub!");
     }
 
@@ -73,7 +76,6 @@ public class AppOpsManager {
         throw new RuntimeException("Stub!");
     }
 
-    /*
     public void startWatchingMode(String op, String packageName, android.app.AppOpsManager.OnOpChangedListener callback) {
         throw new RuntimeException("Stub!");
     }
@@ -81,7 +83,6 @@ public class AppOpsManager {
     public void stopWatchingMode(android.app.AppOpsManager.OnOpChangedListener callback) {
         throw new RuntimeException("Stub!");
     }
-    */
 
     public int checkOp(String op, int uid, String packageName) {
         throw new RuntimeException("Stub!");
@@ -92,10 +93,6 @@ public class AppOpsManager {
     }
 
     public int noteOp(String op, int uid, String packageName) {
-        throw new RuntimeException("Stub!");
-    }
-
-    public int noteOpNoThrow(String op, int uid, String packageName) {
         throw new RuntimeException("Stub!");
     }
 
@@ -119,7 +116,234 @@ public class AppOpsManager {
         throw new RuntimeException("Stub!");
     }
 
+    public interface OnOpChangedListener {
+        void onOpChanged(String var1, String var2);
+    }
+
+    public void setUidMode(int code, int uid, int mode) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    public void setUidMode(String appOp, int uid, int mode) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    public void setUserRestriction(int code, boolean restricted, IBinder token) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    public void setUserRestriction(int code, boolean restricted, IBinder token,
+                                   String[] exceptionPackages) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    public void setUserRestrictionForUser(int code, boolean restricted, IBinder token,
+                                          String[] exceptionPackages, int userId) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    public void setMode(int code, int uid, String packageName, int mode) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    public void setRestriction(int code, int usage, int mode,
+                               String[] exceptionPackages) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    public void resetAllModes() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void startWatchingMode(int op, String packageName, final OnOpChangedListener callback) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public int noteOpNoThrow(String op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public int checkOp(int op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+
+    /**
+     * Like {@link #checkOp} but instead of throwing a {@link SecurityException} it
+     * returns {@link #MODE_ERRORED}.
+     *
+     * @hide
+     */
+    public int checkOpNoThrow(int op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Do a quick check to validate if a package name belongs to a UID.
+     *
+     * @throws SecurityException if the package name doesn't belong to the given
+     *                           UID, or if ownership cannot be verified.
+     */
     public void checkPackage(int uid, String packageName) {
         throw new RuntimeException("Stub!");
     }
+
+    /**
+     * Like {@link #checkOp} but at a stream-level for audio operations.
+     *
+     * @hide
+     */
+    public int checkAudioOp(int op, int stream, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Like {@link #checkAudioOp} but instead of throwing a {@link SecurityException} it
+     * returns {@link #MODE_ERRORED}.
+     *
+     * @hide
+     */
+    public int checkAudioOpNoThrow(int op, int stream, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Make note of an application performing an operation.  Note that you must pass
+     * in both the uid and name of the application to be checked; this function will verify
+     * that these two match, and if not, return {@link #MODE_IGNORED}.  If this call
+     * succeeds, the last execution time of the operation for this app will be updated to
+     * the current time.
+     *
+     * @param op          The operation to note.  One of the OP_* constants.
+     * @param uid         The user id of the application attempting to perform the operation.
+     * @param packageName The name of the application attempting to perform the operation.
+     * @return Returns {@link #MODE_ALLOWED} if the operation is allowed, or
+     * {@link #MODE_IGNORED} if it is not allowed and should be silently ignored (without
+     * causing the app to crash).
+     * @throws SecurityException If the app has been configured to crash on this op.
+     * @hide
+     */
+    public int noteOp(int op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Make note of an application performing an operation on behalf of another
+     * application when handling an IPC. Note that you must pass the package name
+     * of the application that is being proxied while its UID will be inferred from
+     * the IPC state; this function will verify that the calling uid and proxied
+     * package name match, and if not, return {@link #MODE_IGNORED}. If this call
+     * succeeds, the last execution time of the operation for the proxied app and
+     * your app will be updated to the current time.
+     *
+     * @param op                 The operation to note. One of the OPSTR_* constants.
+     * @param proxiedPackageName The name of the application calling into the proxy application.
+     * @return Returns {@link #MODE_ALLOWED} if the operation is allowed, or
+     * {@link #MODE_IGNORED} if it is not allowed and should be silently ignored (without
+     * causing the app to crash).
+     * @throws SecurityException If the proxy or proxied app has been configured to
+     *                           crash on this op.
+     * @hide
+     */
+    public int noteProxyOp(int op, String proxiedPackageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Like {@link #noteProxyOp(int, String)} but instead
+     * of throwing a {@link SecurityException} it returns {@link #MODE_ERRORED}.
+     *
+     * @hide
+     */
+    public int noteProxyOpNoThrow(int op, String proxiedPackageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Like {@link #noteOp} but instead of throwing a {@link SecurityException} it
+     * returns {@link #MODE_ERRORED}.
+     *
+     * @hide
+     */
+    public int noteOpNoThrow(int op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * @hide
+     */
+    public int noteOp(int op) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Report that an application has started executing a long-running operation.  Note that you
+     * must pass in both the uid and name of the application to be checked; this function will
+     * verify that these two match, and if not, return {@link #MODE_IGNORED}.  If this call
+     * succeeds, the last execution time of the operation for this app will be updated to
+     * the current time and the operation will be marked as "running".  In this case you must
+     * later call {@link #finishOp(int, int, String)} to report when the application is no
+     * longer performing the operation.
+     *
+     * @param op          The operation to start.  One of the OP_* constants.
+     * @param uid         The user id of the application attempting to perform the operation.
+     * @param packageName The name of the application attempting to perform the operation.
+     * @return Returns {@link #MODE_ALLOWED} if the operation is allowed, or
+     * {@link #MODE_IGNORED} if it is not allowed and should be silently ignored (without
+     * causing the app to crash).
+     * @throws SecurityException If the app has been configured to crash on this op.
+     * @hide
+     */
+    public int startOp(int op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Like {@link #startOp} but instead of throwing a {@link SecurityException} it
+     * returns {@link #MODE_ERRORED}.
+     *
+     * @hide
+     */
+
+    public int startOpNoThrow(int op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * @hide
+     */
+
+    public int startOp(int op) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * Report that an application is no longer performing an operation that had previously
+     * been started with {@link #startOp(int, int, String)}.  There is no validation of input
+     * or result; the parameters supplied here must be the exact same ones previously passed
+     * in when starting the operation.
+     *
+     * @hide
+     */
+
+    public void finishOp(int op, int uid, String packageName) {
+        throw new RuntimeException("Stub!");
+    }
+
+    /**
+     * @hide
+     */
+
+    public void finishOp(int op) {
+        throw new RuntimeException("Stub!");
+    }
+
+
 }
