692da2fc778e3a8e8e308c101bb63d62eaf87d9a
Nihility <Nihility.t@gmail.com>
2022-12-14 23:44:25 +0800

    remove useless android.app.NotificationManager proxy

--- push/src/main/java/com/xiaomi/xmsf/push/control/NotificationManagerKit.java
+++ push/src/main/java/com/xiaomi/xmsf/push/control/NotificationManagerKit.java
@@ -1,13 +1,8 @@
 package com.xiaomi.xmsf.push.control;
 
-import android.app.Notification;
-import android.app.NotificationManagerExtender;
 import android.content.Context;
 
-import com.elvishew.xlog.Logger;
-import com.elvishew.xlog.XLog;
 import com.oasisfeng.condom.CondomKit;
-
 
 
 /**
@@ -20,46 +15,10 @@ public class NotificationManagerKit implements CondomKit {
     @Override
     public void onRegister(CondomKitRegistry registry) {
         registry.registerSystemService(Context.NOTIFICATION_SERVICE, new CondomKit.SystemServiceSupplier() {
-
             @Override
             public Object getSystemService(Context context, String name) {
-                if (Context.NOTIFICATION_SERVICE.equals(name)) {
-                    return new CondomNotificationManager(context);
-                }
                 return null;
             }
         });
-    }
-
-    static class CondomNotificationManager extends NotificationManagerExtender {
-        private final Logger logger = XLog.tag("CondomNotificationManager").build();
-        CondomNotificationManager(Context context) {
-            super(context);
-        }
-
-        @Override public void notify(final int id, final Notification notification) {
-            logger.d("notify -> " + id + ", " + notification);
-            super.notify(id, notification);
-        }
-
-        @Override public void notify(final String tag, final int id, final Notification notification) {
-            logger.d("notify -> " + tag + ", " + id + ", " + notification);
-            super.notify(tag, id, notification);
-        }
-
-        @Override public void cancel(final int id) {
-            logger.d("cancel -> " + id);
-            super.cancel(id);
-        }
-
-        @Override public void cancel(final String tag, final int id) {
-            logger.d("cancel -> " + tag + ", " + id);
-            super.cancel(tag, id);
-        }
-
-        @Override public void cancelAll() {
-            logger.d("cancelAll");
-            super.cancelAll();
-        }
     }
 }
