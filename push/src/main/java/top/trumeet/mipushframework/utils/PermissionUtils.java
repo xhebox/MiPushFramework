ae733edc38ccc43f94ed07b2884fbe4bb031e406
Nihility <Nihility.t@gmail.com>
2023-01-07 17:00:20 +0800

    refactor: extract PermissionUtils from CheckRunInBackgroundActivity

---
+++ push/src/main/java/top/trumeet/mipushframework/utils/PermissionUtils.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.trumeet.mipushframework.utils;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+import com.xiaomi.xmsf.R;
+
+import top.trumeet.common.Constants;
+import top.trumeet.common.utils.Utils;
+
+public class PermissionUtils {
+    public static boolean canAppOpsPermission() {
+        return Utils.isAppOpsInstalled() ||
+                ShellUtils.isSuAvailable();
+    }
+
+    public static boolean lunchAppOps(Context context, String permission, CharSequence tips) {
+        // root first
+        if (ShellUtils.isSuAvailable()) {
+            if (allowPermission(permission)) {
+                return true;
+            } else {
+                Toast.makeText(context, R.string.fail, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (Utils.isAppOpsInstalled()) {
+            Intent intent = new Intent("rikka.appops.intent.action.PACKAGE_DETAIL")
+                    .addCategory(Intent.CATEGORY_DEFAULT)
+                    .setClassName("rikka.appops", "rikka.appops.DetailActivity")
+                    .putExtra("rikka.appops.intent.extra.USER_HANDLE", Utils.myUid())
+                    .putExtra("rikka.appops.intent.extra.PACKAGE_NAME", Constants.SERVICE_APP_NAME)
+                    .setData(Uri.parse("package:" + Constants.SERVICE_APP_NAME))
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+            Toast.makeText(context, tips, Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean allowPermission(String permission) {
+        return ShellUtils.exec("appops set --user " + Utils.myUid() +
+                " " + Constants.SERVICE_APP_NAME + " " + permission +
+                " " + AppOpsManager.MODE_ALLOWED);
+    }
+}
--- push/src/main/java/top/trumeet/mipushframework/wizard/CheckRunInBackgroundActivity.java
+++ push/src/main/java/top/trumeet/mipushframework/wizard/CheckRunInBackgroundActivity.java
@@ -2,21 +2,18 @@ package top.trumeet.mipushframework.wizard;
 
 import android.app.AppOpsManager;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.android.setupwizardlib.view.NavigationBar;
 import com.xiaomi.xmsf.R;
 
-import top.trumeet.common.Constants;
 import top.trumeet.common.override.AppOpsManagerOverride;
 import top.trumeet.common.utils.Utils;
-import top.trumeet.mipushframework.utils.ShellUtils;
+import top.trumeet.mipushframework.utils.PermissionUtils;
 
 /**
  * Created by Trumeet on 2017/8/25.
@@ -30,7 +27,7 @@ public class CheckRunInBackgroundActivity extends PushControllerWizardActivity i
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N || !canAppOpsPermission()) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N || !PermissionUtils.canAppOpsPermission()) {
             nextPage();
             finish();
             return;
@@ -42,7 +39,7 @@ public class CheckRunInBackgroundActivity extends PushControllerWizardActivity i
     public void onResume() {
         super.onResume();
 
-        if (!canAppOpsPermission()) {
+        if (!PermissionUtils.canAppOpsPermission()) {
             nextPage();
             finish();
             return;
@@ -89,8 +86,8 @@ public class CheckRunInBackgroundActivity extends PushControllerWizardActivity i
 
     @Override
     public void onNavigateNext() {
-        if (!allow && canAppOpsPermission()) {
-            lunchAppOps(
+        if (!allow && PermissionUtils.canAppOpsPermission()) {
+            PermissionUtils.lunchAppOps(this,
                     String.valueOf(AppOpsManagerOverride.OP_RUN_IN_BACKGROUND),
                     Utils.getString(R.string.rikka_appops_help_toast, this));
             return;
@@ -101,42 +98,5 @@ public class CheckRunInBackgroundActivity extends PushControllerWizardActivity i
     private void nextPage() {
         startActivity(new Intent(this,
                 UsageStatsPermissionActivity.class));
-    }
-
-    private boolean canAppOpsPermission() {
-        return Utils.isAppOpsInstalled() ||
-                ShellUtils.isSuAvailable();
-    }
-
-    private boolean lunchAppOps(String permission, CharSequence tips) {
-        // root first
-        if (ShellUtils.isSuAvailable()) {
-            if (allowPermission(permission)) {
-                return true;
-            } else {
-                Toast.makeText(this, R.string.fail, Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        if (Utils.isAppOpsInstalled()) {
-            Intent intent = new Intent("rikka.appops.intent.action.PACKAGE_DETAIL")
-                    .addCategory(Intent.CATEGORY_DEFAULT)
-                    .setClassName("rikka.appops", "rikka.appops.DetailActivity")
-                    .putExtra("rikka.appops.intent.extra.USER_HANDLE", Utils.myUid())
-                    .putExtra("rikka.appops.intent.extra.PACKAGE_NAME", Constants.SERVICE_APP_NAME)
-                    .setData(Uri.parse("package:" + Constants.SERVICE_APP_NAME))
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            Toast.makeText(this, tips, Toast.LENGTH_LONG).show();
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean allowPermission(String permission) {
-        return ShellUtils.exec("appops set --user " + Utils.myUid() +
-                " " + Constants.SERVICE_APP_NAME + " " + permission +
-                " " + AppOpsManager.MODE_ALLOWED);
     }
 }
