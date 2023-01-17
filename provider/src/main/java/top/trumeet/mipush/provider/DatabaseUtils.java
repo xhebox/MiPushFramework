3f89e8b96d6880bfe1a44f00d4d51f29780cf085
Nihility <Nihility.T@gmail.com>
2023-01-17 17:38:10 +0800

    support database downgrade, fix database initialization error

--- provider/src/main/java/top/trumeet/mipush/provider/DatabaseUtils.java
+++ provider/src/main/java/top/trumeet/mipush/provider/DatabaseUtils.java
@@ -50,6 +50,10 @@ public class DatabaseUtils {
                 }
             }, EventDao.class, RegisteredApplicationDao.class);
         }
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
     }
 
 }
