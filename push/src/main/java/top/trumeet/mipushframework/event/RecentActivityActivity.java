e5f7db8833c343e9aef586599f1738eaa21fbf9f
xhe <xw897002528@gmail.com>
2023-02-03 23:47:04 +0800

    searchbar in main activity
    Signed-off-by: xhe <xw897002528@gmail.com>

--- push/src/main/java/top/trumeet/mipushframework/event/RecentActivityActivity.java
+++ push/src/main/java/top/trumeet/mipushframework/event/RecentActivityActivity.java
@@ -6,28 +6,25 @@ import android.view.MenuItem;
 
 /**
  * Created by Trumeet on 2017/8/28.
+ * 
  * @author Trumeet
  */
 
 public class RecentActivityActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate (Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(android.R.id.content,
-                            EventFragment.newInstance(getIntent()
-                            .getDataString()))
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, EventFragment.newInstance(getIntent().getDataString()))
                     .commitAllowingStateLoss();
         }
-        getSupportActionBar()
-                .setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
-    public boolean onOptionsItemSelected (MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             onBackPressed();
             return true;
